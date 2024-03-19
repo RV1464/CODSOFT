4c49d0300f2bcacf6a714a1b3de21f453fd826c1
RV1464 <88540082+RV1464@users.noreply.github.com>
2024-03-19 11:36:04 +0530

    Add files via upload

---
+++ Credit Card Fraud Detection.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+from plotly import tools
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+import gc
+from datetime import datetime 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+import xgboost as xgb
+
+
+# In[2]:
+
+
+credit=pd.read_csv("creditcard.csv")
+
+
+# In[3]:
+
+
+credit.head()
+
+
+# In[4]:
+
+
+credit.info
+
+
+# In[5]:
+
+
+credit.describe()
+
+
+# In[6]:
+
+
+credit.isnull().sum()
+
+
+# In[7]:
+
+
+temp = credit["Class"].value_counts()
+df = pd.DataFrame({'Class': temp.index,'values': temp.values})
+
+trace = go.Bar(
+    x = df['Class'],y = df['values'],
+    name="Credit Card Fraud Class - data unbalance (Not fraud = 0, Fraud = 1)",
+    marker=dict(color="blue"),
+    text=df['values']
+)
+data = [trace]
+layout = dict(title = 'Credit Card Fraud Class - data unbalance (Not fraud = 0, Fraud = 1)',
+          xaxis = dict(title = 'Class', showticklabels=True), 
+          yaxis = dict(title = 'Number of transactions'),
+          hovermode = 'closest',width=600
+         )
+fig = dict(data=data, layout=layout)
+iplot(fig, filename='class')
+
+
+# In[8]:
+
+
+class_0 = credit.loc[credit['Class'] == 0]["Time"]
+class_1 = credit.loc[credit['Class'] == 1]["Time"]
+
+hist_data = [class_0, class_1]
+group_labels = ['Not Fraud', 'Fraud']
+
+fig = ff.create_distplot(hist_data, group_labels, show_hist=False, show_rug=False)
+fig['layout'].update(title='Credit Card Transactions Time Density Plot', xaxis=dict(title='Time [s]'))
+iplot(fig, filename='dist_only')
+
+
+# In[9]:
+
+
+credit['Hour'] = credit['Time'].apply(lambda x: np.floor(x / 3600))
+
+tmp = credit.groupby(['Hour', 'Class'])['Amount'].aggregate(['min', 'max', 'count', 'sum', 'mean', 'median', 'var']).reset_index()
+df = pd.DataFrame(tmp)
+df.columns = ['Hour', 'Class', 'Min', 'Max', 'Transactions', 'Sum', 'Mean', 'Median', 'Var']
+df.head()
+
+
+# In[10]:
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(18,6))
+s = sns.lineplot(ax = ax1, x="Hour", y="Sum", data=df.loc[df.Class==0])
+s = sns.lineplot(ax = ax2, x="Hour", y="Sum", data=df.loc[df.Class==1], color="red")
+plt.suptitle("Total Amount")
+plt.show();
+
+
+# In[11]:
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(18,6))
+s = sns.lineplot(ax = ax1, x="Hour", y="Transactions", data=df.loc[df.Class==0])
+s = sns.lineplot(ax = ax2, x="Hour", y="Transactions", data=df.loc[df.Class==1], color="red")
+plt.suptitle("Total Number of Transactions")
+plt.show();
+
+
+# In[12]:
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(18,6))
+s = sns.lineplot(ax = ax1, x="Hour", y="Mean", data=df.loc[df.Class==0])
+s = sns.lineplot(ax = ax2, x="Hour", y="Mean", data=df.loc[df.Class==1], color="red")
+plt.suptitle("Average Amount of Transactions")
+plt.show();
+
+
+# In[13]:
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(18,6))
+s = sns.lineplot(ax = ax1, x="Hour", y="Max", data=df.loc[df.Class==0])
+s = sns.lineplot(ax = ax2, x="Hour", y="Max", data=df.loc[df.Class==1], color="red")
+plt.suptitle("Maximum Amount of Transactions")
+plt.show();
+
+
+# In[14]:
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(18,6))
+s = sns.lineplot(ax = ax1, x="Hour", y="Median", data=df.loc[df.Class==0])
+s = sns.lineplot(ax = ax2, x="Hour", y="Median", data=df.loc[df.Class==1], color="red")
+plt.suptitle("Median Amount of Transactions")
+plt.show();
+
+
+# In[15]:
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(18,6))
+s = sns.lineplot(ax = ax1, x="Hour", y="Min", data=df.loc[df.Class==0])
+s = sns.lineplot(ax = ax2, x="Hour", y="Min", data=df.loc[df.Class==1], color="red")
+plt.suptitle("Minimum Amount of Transactions")
+plt.show();
+
+
+# In[16]:
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12,6))
+s = sns.boxplot(ax = ax1, x="Class", y="Amount", hue="Class",data=credit, palette="PRGn",showfliers=True)
+s = sns.boxplot(ax = ax2, x="Class", y="Amount", hue="Class",data=credit, palette="PRGn",showfliers=False)
+plt.show();
+
+
+# In[17]:
+
+
+tmp = credit[['Amount','Class']].copy()
+class_0 = tmp.loc[tmp['Class'] == 0]['Amount']
+class_1 = tmp.loc[tmp['Class'] == 1]['Amount']
+class_0.describe()
+
+
+# In[18]:
+
+
+class_1.describe()
+
+
+# In[19]:
+
+
+fraud = credit.loc[credit['Class'] == 1]
+
+trace = go.Scatter(
+    x = fraud['Time'],y = fraud['Amount'],
+    name="Amount",
+     marker=dict(
+                color='rgb(238,23,211)',
+                line=dict(
+                    color='red',
+                    width=1),
+                opacity=0.5,
+            ),
+    text= fraud['Amount'],
+    mode = "markers"
+)
+data = [trace]
+layout = dict(title = 'Amount of fraudulent transactions',
+          xaxis = dict(title = 'Time [s]', showticklabels=True), 
+          yaxis = dict(title = 'Amount'),
+          hovermode='closest'
+         )
+fig = dict(data=data, layout=layout)
+iplot(fig, filename='fraud-amount')
+
+
+# In[20]:
+
+
+plt.figure(figsize = (14,14))
+plt.title('Credit Card Transactions features correlation plot (Pearson)')
+corr = credit.corr()
+sns.heatmap(corr,xticklabels=corr.columns,yticklabels=corr.columns,linewidths=.1,cmap="YlGnBu")
+plt.show()
+
+
+# In[21]:
+
+
+s = sns.lmplot(x='V20', y='Amount',data=credit, hue='Class', fit_reg=True,scatter_kws={'s':2})
+s = sns.lmplot(x='V7', y='Amount',data=credit, hue='Class', fit_reg=True,scatter_kws={'s':2})
+plt.show()
+
+
+# In[22]:
+
+
+var = credit.columns.values
+
+i = 0
+t0 = credit.loc[credit['Class'] == 0]
+t1 = credit.loc[credit['Class'] == 1]
+
+sns.set_style('whitegrid')
+plt.figure()
+fig, ax = plt.subplots(8,4,figsize=(16,28))
+
+for feature in var:
+    i += 1
+    plt.subplot(8,4,i)
+    sns.kdeplot(t0[feature], bw=0.5,label="Class = 0")
+    sns.kdeplot(t1[feature], bw=0.5,label="Class = 1")
+    plt.xlabel(feature, fontsize=12)
+    locs, labels = plt.xticks()
+    plt.tick_params(axis='both', which='major', labelsize=12)
+plt.show();
+
+
+# In[23]:
+
+
+target = 'Class'
+predictors = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',\
+       'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19',\
+       'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28',\
+       'Amount']
+
+
+# In[24]:
+
+
+VALID_SIZE = 0.20 
+TEST_SIZE = 0.20
+RANDOM_STATE = 2018
+NUMBER_KFOLDS = 5
+MAX_ROUNDS = 1000
+EARLY_STOP = 50 
+OPT_ROUNDS = 1000 
+VERBOSE_EVAL = 50 
+train_df, test_df = train_test_split(credit, test_size=TEST_SIZE, random_state=RANDOM_STATE, shuffle=True )
+train_df, valid_df = train_test_split(train_df, test_size=VALID_SIZE, random_state=RANDOM_STATE, shuffle=True )
+
+
+# In[25]:
+
+
+dtrain = xgb.DMatrix(train_df[predictors], train_df[target].values)
+dvalid = xgb.DMatrix(valid_df[predictors], valid_df[target].values)
+dtest = xgb.DMatrix(test_df[predictors], test_df[target].values)
+watchlist = [(dtrain, 'train'), (dvalid, 'valid')]
+
+params = {}
+params['objective'] = 'binary:logistic'
+params['eta'] = 0.039
+params['silent'] = True
+params['max_depth'] = 2
+params['subsample'] = 0.8
+params['colsample_bytree'] = 0.9
+params['eval_metric'] = 'auc'
+params['random_state'] = RANDOM_STATE
+
+
+# In[26]:
+
+
+model = xgb.train(params, 
+                dtrain, 
+                MAX_ROUNDS, 
+                watchlist, 
+                early_stopping_rounds=EARLY_STOP, 
+                maximize=True, 
+                verbose_eval=VERBOSE_EVAL)
+
+
+# In[27]:
+
+
+fig, (ax) = plt.subplots(ncols=1, figsize=(8,5))
+xgb.plot_importance(model, height=0.8, title="Features importance (XGBoost)", ax=ax, color="purple") 
+plt.show()
+
+
+# In[28]:
+
+
+preds = model.predict(dtest)
+roc_auc_score(test_df[target].values, preds)
+
